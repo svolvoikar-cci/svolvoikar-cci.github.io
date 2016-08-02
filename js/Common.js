dd6fd23de0e7b1c0f89e6a5efe332ae897357cc0
svolvoikar-cci <shradha.volvoikar@creativecapsule.com>
2016-08-02 17:40:51 +0530

    Displaying image in canvas with required ratio

--- js/Common.js
+++ js/Common.js
@@ -13,49 +13,41 @@ var sParamSep = "|";
   This func will take your objective c code function and params to it. 
   e.g : document.location = "myapp:" + "myfunction:" + param1 + ":" + param2;
 */
-function makecall(functionname , params)
-{
-	
-	if(params != null)
-	{
-		if(params.length > 0)
-		{
-			var sParams = "";
-			//alert(params.length);
-			for(i = 0;i < params.length; i++)
-			{			
-				
-				sParams += params[i] + sParamSep;
-				
-			}
-			if(sParams.length > 0)
-		    {
-				sParams = sParams.substring(0, sParams.length-1);
-			}
-			//alert(sAppConstant + sHTTPSep + functionname + sParamSep + sParams);
-			window.location =  sAppConstant + sHTTPSep + functionname + sParamSep + sParams;
-		}else
-		{
-       
-			window.location = sAppConstant + sHTTPSep + functionname;
-		}
-	}else
-	{
-       
-	   window.location = sAppConstant + sHTTPSep + functionname;
-		
-	}
+function makecall(functionname, params) {
+
+    if (params != null) {
+        if (params.length > 0) {
+            var sParams = "";
+            //alert(params.length);
+            for (i = 0; i < params.length; i++) {
+
+                sParams += params[i] + sParamSep;
+
+            }
+            if (sParams.length > 0) {
+                sParams = sParams.substring(0, sParams.length - 1);
+            }
+            //alert(sAppConstant + sHTTPSep + functionname + sParamSep + sParams);
+            window.location = sAppConstant + sHTTPSep + functionname + sParamSep + sParams;
+        } else {
+
+            window.location = sAppConstant + sHTTPSep + functionname;
+        }
+    } else {
+
+        window.location = sAppConstant + sHTTPSep + functionname;
+
+    }
 
 
 }
 
-function savetodb()
-{
+function savetodb() {
 
-	var arr = Array();
-	arr[0] = document.frmtest.txtname.value;
-	arr[1] = document.frmtest.txtdtpicker.value;
-	makecall("savedata",arr);
-	
+    var arr = Array();
+    arr[0] = document.frmtest.txtname.value;
+    arr[1] = document.frmtest.txtdtpicker.value;
+    makecall("savedata", arr);
+
 
 }
